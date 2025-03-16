7578c30ef121b13ae9d9066bcb3c45c35339a3b5
Vladislave34 <ostapchukvladislav77@gmail.com>
2025-03-16 09:18:22 +0200

    1

---
+++ DIConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    public static class DIConfiguration
+    {
+        public static IServiceProvider GetServiceProvider()
+        {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    //Прописумо налаштування для різних обєктів
+                    services.AddDbContext<Animal.infrastructure.AppAnimalContext>(options =>
+                        options.UseNpgsql("Host=ep-damp-cell-a2taj2ik-pooler.eu-central-1.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_TnC7QlaA3bWx"));
+
+                    //Якщо у коді потрібно репозіторій, то на основі IRepository буде створюватися
+                    //Repository
+                    services.AddScoped(typeof(Animal.Infrastructure.Interfaces.IRepository<>), typeof(Animal.Infrastructure.Services.Repository<>));
+                    services.AddScoped<Animal.infrastructure.Interface.ISpecieService, Animal.infrastructure.Servises.SpecieService>();
+
+                })
+                .Build();
+
+            //Створити об'єкт score, який згідно налаштувань може видавти різні обєкти по DI
+            var score = host.Services.CreateScope();
+            //Отримуємо провайдер, який може видавати потрібні нам об'єкти
+            return score.ServiceProvider;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,8 +2,13 @@
 using Animal.infrastructure.Servises;
 using Animal.Infrastructure.Entities;
 using Animal.Infrastructure.Services;
+using Microsoft.Extensions.Hosting;
 using System.Text;
-
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Animal.Infrastructure.Interfaces;
+using Animal.infrastructure.Interface;
+using System.Net.WebSockets;
 namespace ConsoleApp36
 {
     internal class Program
@@ -12,28 +17,28 @@ namespace ConsoleApp36
         {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
+
+            var sp = DIConfiguration.GetServiceProvider();
             Console.WriteLine("Передаю вітання нашим друзям Броненосцям. :)!");
-            var context = new AppAnimalContext();
-            var repository = new Repository<Specie>(context);
-            var specieService = new SpecieService(repository, context);
+            var animalService = sp.GetService<IAnimalService>();
 
 
-            
 
-            int choice = 0;
+
+
             while (true)
             {
-                Console.WriteLine("\nВиберіть дію:");
-                Console.WriteLine("1 - Додати вид");
-                Console.WriteLine("2 - Оновити вид");
-                Console.WriteLine("3 - Видалити вид");
-                Console.WriteLine("4 - Показати всі види");
-                Console.WriteLine("5 - Вийти");
-                Console.Write("Ваш вибір: ");
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1. Додати новий вид");
+                Console.WriteLine("2. Оновити вид");
+                Console.WriteLine("3. Видалити вид");
+                Console.WriteLine("4. Переглянути всі види");
+                Console.WriteLine("5. Вихід");
+                Console.Write("Виберіть опцію: ");
 
-                if (!int.TryParse(Console.ReadLine(), out choice))
+                if (!int.TryParse(Console.ReadLine(), out int choice))
                 {
-                    Console.WriteLine("Неправильний ввід! Введіть число.");
+                    Console.WriteLine("Невірний ввід!");
                     continue;
                 }
 
@@ -42,15 +47,15 @@ namespace ConsoleApp36
                     case 1:
                         Console.Write("Введіть назву нового виду: ");
                         string? name = Console.ReadLine();
-                        if (!string.IsNullOrWhiteSpace(name))
+                        Console.Write("Введіть вагу: ");
+                        if (double.TryParse(Console.ReadLine(), out double weight))
                         {
-                            var newSpecie = new Specie { Name = name };
-                            specieService.Add(newSpecie);
-                            Console.WriteLine($"Вид '{name}' успішно додано.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Назва не може бути порожньою!");
+                            Console.Write("Введіть ID категорії виду: ");
+                            if (int.TryParse(Console.ReadLine(), out int specieId))
+                            {
+                                var newSpecie = animalService.CreateSpecie(name!, "", (int)weight, specieId);
+                                Console.WriteLine(newSpecie != null ? "Вид успішно додано." : "Помилка при додаванні.");
+                            }
                         }
                         break;
 
@@ -58,30 +63,18 @@ namespace ConsoleApp36
                         Console.Write("Введіть ID виду для оновлення: ");
                         if (int.TryParse(Console.ReadLine(), out int updateId))
                         {
-                            var specieToUpdate = specieService.GetById(updateId);
+                            var specieToUpdate = animalService.GetSpecieById(updateId);
                             if (specieToUpdate != null)
                             {
                                 Console.Write("Введіть нову назву: ");
                                 string? newName = Console.ReadLine();
-                                if (!string.IsNullOrWhiteSpace(newName))
+                                Console.Write("Введіть нову вагу: ");
+                                if (double.TryParse(Console.ReadLine(), out double newWeight))
                                 {
-                                    specieToUpdate.Name = newName;
-                                    specieService.Update(specieToUpdate);
-                                    Console.WriteLine("Вид оновлено.");
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Назва не може бути порожньою!");
+                                    bool updated = animalService.UpdateSpecie(updateId, newName!, "", (int)newWeight, specieToUpdate.SpecieId);
+                                    Console.WriteLine(updated ? "Вид оновлено." : "Помилка при оновленні.");
                                 }
                             }
-                            else
-                            {
-                                Console.WriteLine("Вид з таким ID не знайдено!");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Неправильний ID!");
                         }
                         break;
 
@@ -89,40 +82,132 @@ namespace ConsoleApp36
                         Console.Write("Введіть ID виду для видалення: ");
                         if (int.TryParse(Console.ReadLine(), out int deleteId))
                         {
-                            try
-                            {
-                                specieService.Delete(deleteId);
-                                Console.WriteLine($"Вид з ID {deleteId} успішно видалено.");
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine($"Помилка: {ex.Message}");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Неправильний ID!");
+                            Console.WriteLine(animalService.DeleteSpecie(deleteId) ? "Вид успішно видалено." : "Помилка при видаленні.");
                         }
                         break;
 
                     case 4:
                         Console.WriteLine("Список всіх видів:");
-                        var species = specieService.GetAll();
-                        foreach (var speci in species)
+                        foreach (var speci in animalService.GetAllSpecies())
                         {
-                            Console.WriteLine($"ID: {speci.Id}, Назва: {speci.Name}");
+                            Console.WriteLine($"ID: {speci.Id}, Назва: {speci.Name}, Вага: {speci.Weight} кг");
                         }
                         break;
 
                     case 5:
-                        Console.WriteLine("Вихід...");
                         return;
 
                     default:
-                        Console.WriteLine("Невідома команда! Спробуйте ще раз.");
+                        Console.WriteLine("Невідома команда!");
                         break;
                 }
             }
+
+
+            //int choice = 0;
+            //while (true)
+            //{
+            //    Console.WriteLine("\nВиберіть дію:");
+            //    Console.WriteLine("1 - Додати вид");
+            //    Console.WriteLine("2 - Оновити вид");
+            //    Console.WriteLine("3 - Видалити вид");
+            //    Console.WriteLine("4 - Показати всі види");
+            //    Console.WriteLine("5 - Вийти");
+            //    Console.Write("Ваш вибір: ");
+
+            //    if (!int.TryParse(Console.ReadLine(), out choice))
+            //    {
+            //        Console.WriteLine("Неправильний ввід! Введіть число.");
+            //        continue;
+            //    }
+
+            //switch (choice)
+            //{
+            //    case 1:
+            //        Console.Write("Введіть назву нового виду: ");
+            //        string? name = Console.ReadLine();
+            //        if (!string.IsNullOrWhiteSpace(name))
+            //        {
+            //            var newSpecie = new Specie { Name = name };
+            //            specieService.Add(newSpecie);
+            //            Console.WriteLine($"Вид '{name}' успішно додано.");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Назва не може бути порожньою!");
+            //        }
+            //        break;
+
+            //    case 2:
+            //        Console.Write("Введіть ID виду для оновлення: ");
+            //        if (int.TryParse(Console.ReadLine(), out int updateId))
+            //        {
+            //            var specieToUpdate = specieService.GetById(updateId);
+            //            if (specieToUpdate != null)
+            //            {
+            //                Console.Write("Введіть нову назву: ");
+            //                string? newName = Console.ReadLine();
+            //                if (!string.IsNullOrWhiteSpace(newName))
+            //                {
+            //                    specieToUpdate.Name = newName;
+            //                    specieService.Update(specieToUpdate);
+            //                    Console.WriteLine("Вид оновлено.");
+            //                }
+            //                else
+            //                {
+            //                    Console.WriteLine("Назва не може бути порожньою!");
+            //                }
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("Вид з таким ID не знайдено!");
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Неправильний ID!");
+            //        }
+            //        break;
+
+            //    case 3:
+            //        Console.Write("Введіть ID виду для видалення: ");
+            //        if (int.TryParse(Console.ReadLine(), out int deleteId))
+            //        {
+            //            try
+            //            {
+            //                specieService.Delete(deleteId);
+            //                Console.WriteLine($"Вид з ID {deleteId} успішно видалено.");
+            //            }
+            //            catch (Exception ex)
+            //            {
+            //                Console.WriteLine($"Помилка: {ex.Message}");
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Неправильний ID!");
+            //        }
+            //        break;
+
+            //    case 4:
+            //        Console.WriteLine("Список всіх видів:");
+            //        var species = specieService.GetAll();
+            //        foreach (var speci in species)
+            //        {
+            //            Console.WriteLine($"ID: {speci.Id}, Назва: {speci.Name}");
+            //        }
+            //        break;
+
+            //    case 5:
+            //        Console.WriteLine("Вихід...");
+            //        return;
+
+            //    default:
+            //        Console.WriteLine("Невідома команда! Спробуйте ще раз.");
+            //        break;
+            //}
+            //}
+
         }
     }
 }
